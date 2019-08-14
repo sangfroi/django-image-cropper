450ae11bc74f54252d846a0065fd261c95cb7578
lwairore <kwairore@gmail.com>
2019-08-14 22:16:22 +0300

    In photo_list view function change 'from = PhotoForm(request.POST, request.FILES)' to 'form = PhotoForm(request.POST, request.FILES)'.

--- resize/views.py
+++ resize/views.py
@@ -6,11 +6,10 @@ from .forms import PhotoForm
 def photo_list(request):
     photos = Photo.objects.all()
     if request.method == "POST":
-        from = PhotoForm(request.POST, request.FILES)
+        form = PhotoForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('photo_list')
     else:
         form = PhotoForm()
     return render(request, 'album/photo_list.html', {'form':form, 'photos': photos})
-    
