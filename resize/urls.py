fd37f9eae440e4e4a6d894d4dde8704131e67017
lwairore <kwairore@gmail.com>
2019-08-14 21:04:53 +0300

    Use an if statement to check if DEBUG in settings is set to TRUE. If so, add a media url that maps to a specific image.

--- resize/urls.py
+++ resize/urls.py
@@ -6,3 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^$', views.photo_list, name="photo_list"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA, document_root=settings.MEDIA_ROOT)
