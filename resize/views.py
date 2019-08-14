ff4cec89b69d7c2646cb2040dacb90681846eea6
lwairore <kwairore@gmail.com>
2019-08-14 20:41:40 +0300

    Define photo_list view function that takes request as an argument. Inside photo_list function: instantiate photos instance that stores all photos store in Photo class. Use an if statement to check if a request to photo_list function is a POST. Use is_valid to validate form instance. Call the save method to save the image.

--- resize/views.py
+++ resize/views.py
@@ -3,3 +3,14 @@ from .models import Photo
 from .forms import PhotoForm
 
 # Create your views here.
+def photo_list(request):
+    photos = Photo.objects.all()
+    if request.method == "POST":
+        from = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('photo_list')
+    else:
+        form = PhotoForm()
+    return render(request, 'album/photo_list.html', {'form':form, 'photos': photos})
+    
