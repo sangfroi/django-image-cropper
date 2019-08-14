53f738750e03390326caf64b3ec01d7cb17ff656
lwairore <kwairore@gmail.com>
2019-08-14 17:05:44 +0300

    Change 'ENGINE' of 'DATABASES' to 'django.db.backends.postgresql' from 'django.db.backends.sqllite3', set NAME to the 'image_cropper', USER to <database username> and PASSWORD to <database passwprd>.

--- cropper/settings.py
+++ cropper/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'cropper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "image_cropper",
+        'USER': "karangu",
+        'PASSWORD': "lmzongolo8754"
     }
 }
 
