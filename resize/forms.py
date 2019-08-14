d58c010bb2bf65008792907b6995dc25890bc24e
lwairore <kwairore@gmail.com>
2019-08-14 20:24:44 +0300

    Create class PhotoForm that to 'ModelForm' that has: x, y , width and height columns that are have FloatField with HiddenInput. Inside class, PhotoForm, define class Meta that sets model to Photo and fields to: file, x, y , width and height.

--- resize/forms.py
+++ resize/forms.py
@@ -2,3 +2,13 @@ from PIL import Image
 from django import forms
 from django.core.files import File
 from .models import Photo
+
+class PhotoForm(forms.ModelForm):
+    x = forms.FloatField(widget=forms.HiddenInput())
+    y = forms.FloatField(widget=forms.HiddenInput())
+    width = forms.FloatField(widget=forms.HiddenInput())
+    height = forms.FloatField(widget=forms.HiddenInput())
+
+    class Meta:
+        model = Photo
+        fields = ('file', 'x', 'y', 'width', 'height',)
